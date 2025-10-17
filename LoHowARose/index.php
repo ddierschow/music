058d10fc42d6c6b8786139215fe1cb608a730648
Dean Dierschow <ddierschow@xocolatl.com>
2025-10-16 23:49:47 -0700

    Lo How A Rose: Changes after third reading.

--- LoHowARose/index.php
+++ LoHowARose/index.php
@@ -11,24 +11,12 @@ $parts = [
     "StringBass", "Mallets", "Timpani"
 ];
 
-echo "<table><tr>\n";
-
-echo "<td>\n";
-show_left_box("lo how a rose e'er blooming", ['Score', 'ScoreNT', 'ScoreSATB'],
-              ['ScoreMidi.mp3', 'First_Reading.mp3', 'Second_Read.mp3']);
-echo "</td>\n";
-
-echo "<td>\n";
-show_parts($parts, 1, 'parts/');
-echo "</td>";
-
-echo "<td>\n";
-show_image();
-echo "<h4>To do</h4><ul>\n";
-echo "<li>write a condensed score (someday?)";
-echo "</ul>\n";
-
-echo "</td></tr></table>\n";
+show_whole_page("lo how a rose e'er blooming", ['Score', 'ScoreNT', 'ScoreSATB'], $parts,
+                ['ScoreMidi.mp3', 'First_Reading.mp3', 'Second_Reading.mp3', 'Third_Reading.mp3'],
+                [['https://en.wikipedia.org/wiki/Es_ist_ein_Ros_entsprungen', 'Wiki page'],
+                 ['https://www.vandagriff.org/wp-content/uploads/2018/08/lo-how-a-rose.jpg', 'Hymnal 1'],
+                 ['https://hymnary.org/page/fetch/WAR2003/205/low/190', 'Hymnal 2']], [],
+                ['write a condensed score (someday?)']);
 
 show_bottom();
 ?>
